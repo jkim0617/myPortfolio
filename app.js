df4750d2ef3c0c8457f56be6fe866d54b9ed9e3c
jkim0617 <jonkim0617@gmail.com>
2025-08-11 17:07:46 -0700

    colorscheme and project section revision

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
-const card = document.getElementsByClassName("project-box")
-const cardImg = document.getElementsByClassName("card-image");
+// const card = document.getElementsByClassName("project-box")
+// const cardImg = document.getElementsByClassName("card-image");
 
-const slotCard = document.querySelector("#slot-card");
-const attendanceCard = document.querySelector("#attendance-card");
-const lfrCard = document.querySelector("#lfr-card");
-const irrigationSysCard = document.querySelector("#irrigation-system-card");
+// const slotCard = document.querySelector("#slot-card");
+// const attendanceCard = document.querySelector("#attendance-card");
+// const lfrCard = document.querySelector("#lfr-card");
+// const irrigationSysCard = document.querySelector("#irrigation-system-card");
 
-slotCard.addEventListener('mouseenter', () => {
-  slotCard.style.backgroundColor = 'red';
-})
-attendanceCard.addEventListener('mouseenter', () => {
-  attendanceCard.style.backgroundColor = '#D2996C';
-})
-lfrCard.addEventListener('mouseenter', () => {
-  lfrCard.style.backgroundColor = 'darkblue';
-})
-irrigationSysCard.addEventListener('mouseenter', () => {
-  irrigationSysCard.style.backgroundColor = 'darkgreen';
-})
-// -------------------------------
-slotCard.addEventListener('mouseout', () => {
-  slotCard.style.backgroundColor = '#de6262';
-})
-attendanceCard.addEventListener('mouseout', () => {
-  attendanceCard.style.backgroundColor = '#f3dbbc';
-})
-lfrCard.addEventListener('mouseout', () => {
-  lfrCard.style.backgroundColor = '#1370fb';
-})
-irrigationSysCard.addEventListener('mouseout', () => {
-  irrigationSysCard.style.backgroundColor = '#6ab100';
-})
+// slotCard.addEventListener('mouseenter', () => {
+//   slotCard.style.backgroundColor = 'red';
+// })
+// attendanceCard.addEventListener('mouseenter', () => {
+//   attendanceCard.style.backgroundColor = '#D2996C';
+// })
+// lfrCard.addEventListener('mouseenter', () => {
+//   lfrCard.style.backgroundColor = 'darkblue';
+// })
+// irrigationSysCard.addEventListener('mouseenter', () => {
+//   irrigationSysCard.style.backgroundColor = 'darkgreen';
+// })
+// // -------------------------------
+// slotCard.addEventListener('mouseout', () => {
+//   slotCard.style.backgroundColor = '#de6262';
+// })
+// attendanceCard.addEventListener('mouseout', () => {
+//   attendanceCard.style.backgroundColor = '#f3dbbc';
+// })
+// lfrCard.addEventListener('mouseout', () => {
+//   lfrCard.style.backgroundColor = '#1370fb';
+// })
+// irrigationSysCard.addEventListener('mouseout', () => {
+//   irrigationSysCard.style.backgroundColor = '#6ab100';
+// })
 
-for (let i = 0; i < card.length; i++) {
-  card[i].addEventListener("mouseenter", () => {
-    cardImg[i].style.display = "none"
-  })
-  card[i].addEventListener("mouseout", () => {
-    cardImg[i].style.display = ""
-  })
-}
+// for (let i = 0; i < card.length; i++) {
+//   card[i].addEventListener("mouseenter", () => {
+//     cardImg[i].style.display = "none"
+//   })
+//   card[i].addEventListener("mouseout", () => {
+//     cardImg[i].style.display = ""
+//   })
+// }
 
-console.log(card);
-console.log(cardImg)
+const projectBox = document.getElementsByClassName("project-box")
+const projectName = document.getElementsByClassName("project-name")
+
+// for (let i = 0; i < projectBox.length; i++) {
+//   projectBox[i].addEventListener('mouseenter', () => {
+//     projectBox[i].style = "background:#8d99ae"
+//     projectName[i].style = "color:#2b2d42";
+//   })
+//   projectBox[i].addEventListener('mouseout', () => {
+//     projectBox[i].style = "background:#2b2d42";
+//     projectName[i].style = "color:#fcf6ea";
+//   })
+// }
+
